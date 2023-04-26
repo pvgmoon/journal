3ed9a0104f0d84a16a4d0fd74f1af8f4a45604a2
Paula <pandreavanegasg@gmail.com>
2023-04-26 12:29:49 -0500

    Programacion de las tareas y el check de verificacion

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,26 +1,50 @@
-const btn = document.querySelector('[data-form-btn]');
+( () => {
 
-const createTask = (evento)=>{
-    evento.preventDefault();
-    const input = document.querySelector('[data-form-input]');
-    const value = input.value;
-    const list = document.querySelector('[data-list]');
-    const task = document.createElement('li');
-    task.classList.add("card");
-    
-    input.value = "";
-    const content = `<div>
-        <i class="far fa-check-square icon"></i>
-        <span class="task">${value}</span>
-    </div>
-    <i class="fas fa-trash-alt trashIcon icon"></i> `;
-    task.innerHTML = content;
+    const btn = document.querySelector('[data-form-btn]');
 
-    list.appendChild(task);
-    console.log(content);
-}
-console.log("btn");
+    const createTask = (evento)=>{
+        evento.preventDefault();
+        const input = document.querySelector('[data-form-input]');
+        const value = input.value;
+        const list = document.querySelector('[data-list]');
+        const task = document.createElement('li');
+        task.classList.add("card");
+        input.value = "";
 
-btn.addEventListener('click', createTask)
+        const taskContent = document.createElement("div");
+        taskContent.appendChild(checkComplete());
+        const titleTask = document.createElement("span");
+        titleTask.classList.add("task");
+        titleTask.innerText = value;
+        taskContent.appendChild(titleTask);
+    /* const i2 = document.createElement("i");
+        i2.classList.add("far", "fa-trash-alt", "trashIcon", "icon");
+        taskContent.appendChild(i2);*/
 
+        const content = `
+        <i class="fas fa-trash-alt trashIcon icon"></i> `;
+    task.appendChild(taskContent);
+        // task.innerHTML = content;
 
+        list.appendChild(task);
+        console.log(content);
+    }
+
+    const checkComplete =() => {
+        const i = document.createElement("i");
+        i.classList.add("far", "fa-check-square", "icon");
+        i.addEventListener("click", completeTask);
+        return i;
+    }
+
+    const completeTask = (event) => {
+        const element=  event.target;
+        element.classList.toggle("fas");
+        element.classList.toggle("completeIcon");
+        element.classList.toggle("icon")
+
+    }
+
+    btn.addEventListener('click', createTask)
+
+}) ();
